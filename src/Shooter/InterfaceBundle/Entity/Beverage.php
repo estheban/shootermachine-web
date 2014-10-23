09ade0dad3ad413519ce55c196a461c71145207a
stremblay <stremblay@phpcreation.com>
2014-10-22 22:38:03 -0400

    Génération entités

---
+++ src/Shooter/InterfaceBundle/Entity/Beverage.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Shooter\InterfaceBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Beverage
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Shooter\InterfaceBundle\Entity\BeverageRepository")
+ */
+class Beverage
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="sku", type="string", length=255)
+     */
+    private $sku;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="time", type="decimal")
+     */
+    private $time;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Beverage
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set sku
+     *
+     * @param string $sku
+     * @return Beverage
+     */
+    public function setSku($sku)
+    {
+        $this->sku = $sku;
+
+        return $this;
+    }
+
+    /**
+     * Get sku
+     *
+     * @return string 
+     */
+    public function getSku()
+    {
+        return $this->sku;
+    }
+
+    /**
+     * Set time
+     *
+     * @param string $time
+     * @return Beverage
+     */
+    public function setTime($time)
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    /**
+     * Get time
+     *
+     * @return string 
+     */
+    public function getTime()
+    {
+        return $this->time;
+    }
+}
---
+++ src/Shooter/InterfaceBundle/Entity/Pump.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Shooter\InterfaceBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Pump
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Shooter\InterfaceBundle\Entity\PumpRepository")
+ */
+class Pump
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="pin", type="integer")
+     */
+    private $pin;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="active", type="boolean")
+     */
+    private $active;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set pin
+     *
+     * @param integer $pin
+     * @return Pump
+     */
+    public function setPin($pin)
+    {
+        $this->pin = $pin;
+
+        return $this;
+    }
+
+    /**
+     * Get pin
+     *
+     * @return integer 
+     */
+    public function getPin()
+    {
+        return $this->pin;
+    }
+
+    /**
+     * Set active
+     *
+     * @param boolean $active
+     * @return Pump
+     */
+    public function setActive($active)
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    /**
+     * Get active
+     *
+     * @return boolean 
+     */
+    public function getActive()
+    {
+        return $this->active;
+    }
+}
