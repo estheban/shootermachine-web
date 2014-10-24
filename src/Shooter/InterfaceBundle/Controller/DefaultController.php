1143bcdd8e80b29be0029a9a3fe87dd7d9dfed0a
stremblay <stremblay@phpcreation.com>
2014-10-24 19:31:23 -0400

    Scequence de pump simple fonctionnel

--- src/Shooter/InterfaceBundle/Controller/DefaultController.php
+++ src/Shooter/InterfaceBundle/Controller/DefaultController.php
@@ -47,15 +47,61 @@ class DefaultController extends Controller
     public function startupAction()
     {
         $arduino = $this->initArduino();
+        $childs = array();
         
         //getActivePumps
         for ($i = 2; $i <= 11; $i++) {
             $arduino->arduinoResquest($arduino->getArduinoServer(), $arduino->initInstruc($i));
+            $childs[] = $arduino->arduinoResquest(
+                            $arduino->getArduinoServer(), 
+                            $arduino->pumpDeactivate($i));
         }
         
-        die('test startup');
+        usleep(10000000);
+        die('Fn startup');
         return array();
     }
+    
+    /**
+     * @Route("/test")
+     * @Template()
+     */
+    public function testAction()
+    {
+        echo 'Start'.'<br />';
+        if (($pid = pcntl_fork()) == 0) {
+            die('Child die : '.$pid.'<br />');
+            // return file_get_contents($arduinoServer . $request);
+        }
+        
+        echo 'Parent :'.$pid.'<br />';
+        
+        phpinfo();
+        
+        die('test startup'.'<br />');
+        return array();
+    }
+    
+    /**
+     * @Route("/ssh")
+     * @Template()
+     */
+    public function sshAction()
+    {
+        require __DIR__.'../Resources/phpseclib0.3.8/Net/SSH2.php';
+
+        $ssh = new Net_SSH2('$this->arduinoServer');
+        if (!$ssh->login('root', 'arduino')) {
+            exit('Login Failed');
+        }
+        
+        echo $ssh->exec('pwd');
+        echo $ssh->exec('ls -la');
+        
+        die('test ssh'.'<br />');
+        return array();
+    }
+   
     
     private function initArduino()
     {
@@ -77,58 +123,115 @@ class DefaultController extends Controller
 // Logic
 // Arduino -> Pump -> Breuvage 
 // fn getTime
-        
+// 
+        //Conversion portion to time
         
         $portions = array();
-        $portions[] = new Portion(2,1.000);
-        $portions[] = new Portion(3,1.000);
-        $portions[] = new Portion(4,1.000);
-        $portions[] = new Portion(5,1.000);
-        $portions[] = new Portion(5,1.000);
-        $portions[] = new Portion(6,1.000);
-        $portions[] = new Portion(7,1.000);
-        $portions[] = new Portion(8,1.000);
-        $portions[] = new Portion(9,1.000);
-        $portions[] = new Portion(10,1.000);
-        $portions[] = new Portion(11,1.000);
+        $portions[] = new Portion(2,0.000);
+        $portions[] = new Portion(3,0.000);
+        $portions[] = new Portion(4,0.000);
+        $portions[] = new Portion(5,0.000);
+        $portions[] = new Portion(6,0.000);
+        $portions[] = new Portion(7,0.000);
+        $portions[] = new Portion(8,0.000);
+        $portions[] = new Portion(9,0.000);
+        $portions[] = new Portion(10,0.000);
+        $portions[] = new Portion(11,0.000);
+
+        foreach ($portions as $key => $portion) {
+            $childs[] = $arduino->arduinoResquest(
+                    $arduino->getArduinoServer(), 
+                    $arduino->pumpActivate($portion->pump_id));
+            $portion->start();
+            echo 'Pump Start :'.$portion->pump_id.'<br />';
+            $i = true;
+            while($i) {
+                usleep(100000);
+                if ($portion->needToStop()) {
+                $childs[] = $arduino->arduinoResquest(
+                        $arduino->getArduinoServer(), 
+                        $arduino->pumpDeactivate($portion->pump_id));
+                echo 'Pump End :'.$portion->pump_id.'<br />';
+                unset($portions[$key]);
+                $i = false;
+                }
+            }
+        }
         
-        
+    }
+
+    public function pumpMultiTread() 
+    {
         //DO IT
+        $childs = array();
 
         //Sort ???
 
         //Start
         echo 'start';
         echo '<br />';
+
         foreach ($portions as $portion) {
-            if($portion->needToStop()) {
-                echo $arduino->arduinoResquest(
-                        $arduino->getArduinoServer(), 
-                        $arduino->pumpActivate($portion->pump_id));
-                $portion->start();
-            } 
+            $childs[] = $arduino->arduinoResquest(
+                    $arduino->getArduinoServer(), 
+                    $arduino->pumpActivate($portion->pump_id));
+            $portion->start();
+            echo 'Pump Start :'.$portion->pump_id.'<br />';
         }
-        
-        echo '<br />';
-        
+
+        //var_dump($childs);
+//        echo 'Wait process start'.'<br />';
+//        while(count($childs) > 0) {
+//            echo 'wait' . count($childs) . '<br />';
+//            usleep(100000);
+//            foreach($childs as $key => $pid) {
+//                $res = pcntl_waitpid($pid, $status, WNOHANG);
+//
+//                // If the process has already exited
+//                if($res == -1 || $res > 0) {
+//                    unset($childs[$key]);
+//                }
+//            }
+//        }
+//        echo '<br />';
+
         //End
-        while(count($portions) >= 1) {
-            echo 'number ' . count($portions);
+        while(count($portions) > 0) {
+            echo 'number ' . count($portions).'<br />';
+            usleep(100000);
             foreach ($portions as $key => $portion) {
                 if($portion->needToStop()) {
-                    echo $arduino->arduinoResquest(
+                    $childs[] = $arduino->arduinoResquest(
                             $arduino->getArduinoServer(), 
                             $arduino->pumpDeactivate($portion->pump_id));
-                    echo '<br />';
+                    echo 'Pump End :'.$portion->pump_id.'<br />';
                     unset($portions[$key]);
+                    //var_dump($portions);
                 } 
             }
         }
-        
+
+        //var_dump($childs);
+//        echo 'Wait process end'.'<br />';
+//        while(count($childs) > 0) {
+//            foreach($childs as $key => $pid) {
+//                $res = pcntl_waitpid($pid, $status, WNOHANG);
+//
+//                // If the process has already exited
+//                if($res == -1 || $res > 0)
+//                    unset($childs[$key]);
+//            }
+//        }
+//        echo '<br />';
+
+        echo 'Master wait'.'<br />';
+        usleep(10000000);
+        die('The end');
         return true;
     }
 
 }
+
 
 class Portion
 {
--- src/Shooter/InterfaceBundle/Entity/Arduino.php
+++ src/Shooter/InterfaceBundle/Entity/Arduino.php
@@ -25,8 +25,32 @@ class Arduino
 	public function arduinoResquest($arduinoServer, $request)
 	{
             $curl = curl_init($arduinoServer . $request);
-            return curl_exec($curl);
-            // return file_get_contents($arduinoServer . $request);;
+            $curl_feedback = curl_exec($curl);
+            
+            return $curl_feedback;
+            
+//            $pid = pcntl_fork();
+//            if($pid == -1)
+//                die('Could not fork');
+//
+//            if ($pid) {
+//                echo 'parent '.$pid;
+//                //pcntl_wait($status);
+//                return $pid;
+//            } else {
+//                // Sleep $i+1 (s). The child process can get this parameters($i).
+//                //Curl is 9x time faster file_get_contents 
+//                //ex : file_get_contents($arduinoServer . $request);
+//                $curl = curl_init($arduinoServer . $request);
+//                $curl_feedback = curl_exec($curl);
+//                //echo $curl_feedback.'<br />';
+//                //echo 'Child die : '.$pid;
+//                
+//                // The child process needed to end the loop.
+//                exit();
+//            }
+//            
+//            return $pid;
 	}
 
 	public function initInstruc($pumpID, $mode = 'output')
